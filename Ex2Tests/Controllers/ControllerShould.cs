05ab3187ca0e0ae8a1ed96cc9daf2ca5a4d50a7a
Frederic Latreille <116024774+Fredkaos@users.noreply.github.com>
2023-05-03 14:36:46 -0400

    Tests Reeussi

--- Ex2Tests/Controllers/ControllerShould.cs
+++ Ex2Tests/Controllers/ControllerShould.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using PremierProjetASP.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,69 @@ using System.Threading.Tasks;
 
 namespace Ex2Tests.Controllers
 {
-    internal class ControllerShould
+    public class ControllerShould
     {
+        [Fact] // Type de test
+        public void NumeroUn() // Methode de test
+        {
+            // Creer le controlleur
+            var homeController = new HomeController();
+
+            // Appeler le methode d'action a tester
+            var result = homeController.Numero1();
+
+            // Verifier le type du resultat
+            var textResult = Assert.IsType<string>(result);
+        }
+
+        [Fact] 
+        public void NumeroDeux()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.Numero2();
+
+            var textResult = Assert.IsType<ContentResult>(result);
+        }
+
+        [Fact]
+        public void NumeroTrois()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.Numero3();
+
+            var textResult = Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void NumeroQuatre()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.Numero4();
+
+            var textResult = Assert.IsAssignableFrom<FileResult>(result);
+        }
+
+        [Fact]
+        public void NumeroCinq()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.Numero5();
+
+            var textResult = Assert.IsType<ContentResult>(result);
+        }
+
+        [Fact]
+        public void NumeroSix()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.Numero6();
+
+            var textResult = Assert.IsType<string>(result);
+        }
     }
 }
