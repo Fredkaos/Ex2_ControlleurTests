32912a3ec9b35bd5860a8ba3978106ca872ac2c0
Frederic Latreille <116024774+Fredkaos@users.noreply.github.com>
2023-05-04 14:27:56 -0400

    Corriger prof/ tout fonctionne

--- Ex2Tests/Controllers/ControllerShould.cs
+++ Ex2Tests/Controllers/ControllerShould.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PremierProjetASP.Controllers;
+using PremierProjetASP.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,108 @@ namespace Ex2Tests.Controllers
 {
     public class ControllerShould
     {
+        [Fact]
+        public void ShowTextString()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.Numero1();
+
+            var expectedResult = "<h2>Bienvenu dans mon site web</h2>";
+            Assert.IsType<string>(result);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void ReturnContent()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.Numero2();
+
+            Assert.IsType<ContentResult>(result);
+
+            var expectedContent = "<h2>Bienvenu dans mon site web</h2>";
+            var content = Assert.IsType<string>(result.Content);
+            Assert.Equal(expectedContent, content);
+
+            var expectedContentType = "text/html";
+            var contentType = Assert.IsType<string>(result.ContentType);
+            Assert.Equal(expectedContentType, contentType);
+        }
+
+        [Fact]
+        public void IndexShouldReturnViewResult()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.Numero3();
+
+            Assert.IsType<ViewResult>(result);
+
+        }
+
+        [Fact]
+        public void AfficherPdfShouldReturnVirtualFileResult()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.Numero4();
+
+            Assert.IsType<VirtualFileResult>(result);
+
+            var expectedFileName = "~/pdf/Chapitre1.pdf";
+            Assert.Equal(expectedFileName, result.FileName);
+
+            var expectedContentType = "application/pdf";
+            Assert.Equal(expectedContentType, result.ContentType);
+        }
+
+        [Fact]
+        public void AfficherJsonShouldReturnJsonResult()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.Numero5();
+
+            Assert.IsType<JsonResult>(result);
+
+            dynamic valueJson = result.Value;
+
+            Client expectedClient1 = new Client(1, "Dupont", "Jean", "jean.dupont@example.com");
+            Client expectedClient2 = new Client(2, "Tremblay", "Marie", "marie.tremblay@example.com");
+
+            var jsonElement1 = valueJson[0];
+            var jsonElement2 = valueJson[1];
+
+            Assert.IsType<Client>(jsonElement1);
+            Assert.IsType<Client>(jsonElement2);
+
+            Assert.Equal<int>(jsonElement1.Numero, expectedClient1.Numero);
+            Assert.Equal(jsonElement1.Nom, expectedClient1.Nom);
+            Assert.Equal(jsonElement1.Prenom, expectedClient1.Prenom);
+            Assert.Equal(jsonElement1.Courriel, expectedClient1.Courriel);
+
+            Assert.Equal<int>(expectedClient2.Numero, jsonElement2.Numero);
+            Assert.Equal(expectedClient2.Nom, jsonElement2.Nom);
+            Assert.Equal(expectedClient2.Prenom, jsonElement2.Prenom);
+            Assert.Equal(expectedClient2.Courriel, jsonElement2.Courriel);
+        }
+
+        [Fact]
+        public void RedirigerShouldReturnStringActionResult()
+        {
+            var homeController = new HomeController();
+
+            var result = homeController.Numero6();
+            var viewResult = Assert.IsType<RedirectToActionResult>(result);
+
+            var expectedActionName = "Numero1";
+            var actionName = Assert.IsType<string>(viewResult.ActionName);
+            Assert.Equal(expectedActionName, actionName);
+        }
+
+        /*
         [Fact] // Type de test
         public void NumeroUn() // Methode de test
         {
@@ -50,7 +153,7 @@ namespace Ex2Tests.Controllers
 
             var result = homeController.Numero4();
 
-            Assert.IsAssignableFrom<FileResult>(result);
+            Assert.IsAssignableFrom<VirtualFileResult>(result);
         }
 
         [Fact]
@@ -60,7 +163,7 @@ namespace Ex2Tests.Controllers
 
             var result = homeController.Numero5();
 
-            Assert.IsType<ContentResult>(result);
+            Assert.IsType<JsonResult>(result);
         }
 
         [Fact]
@@ -72,5 +175,6 @@ namespace Ex2Tests.Controllers
 
             Assert.IsType<string>(result);
         }
+        */
     }
 }
--- PremierProjetASP/Controllers/HomeController.cs
+++ PremierProjetASP/Controllers/HomeController.cs
@@ -23,30 +23,29 @@ namespace PremierProjetASP.Controllers
         {
             return View("Numero3");
         }
-        public FileResult Numero4()
+        public VirtualFileResult Numero4()
         {
             var filePath = "~/pdf/Chapitre1.pdf";
             return File(filePath, "application/pdf");
         }
-        public ContentResult Numero5()
+        public JsonResult Numero5()
         {
             // Créez une liste d'objets clients
             var clients = new List<Client>
             {
                 new Client(1, "Dupont", "Jean", "jean.dupont@example.com"),
                 new Client(2, "Tremblay", "Marie", "marie.tremblay@example.com"),
-                new Client(3, "Gagnon", "Luc", "luc.gagnon@example.com")
             };
 
-            // Sérialiser la liste au format JSON
-            var json = JsonSerializer.Serialize(clients);
+            var options = new JsonSerializerOptions { WriteIndented= true };
+           
+            JsonResult listJSon = Json(clients, options);
 
-            // Afficher la liste sur le navigateur
-            return Content(json, "application/json");
+            return listJSon;
         }
-        public string Numero6()
+        public RedirectToActionResult Numero6()
         {
-            return Numero1();
+            return RedirectToAction("Numero1");
         }
     }
 }
